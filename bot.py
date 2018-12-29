7b240e63016c30d3992db12b74b6595cc2b32af6
artslob <artslob@yandex.ru>
2018-12-30 00:13:06 +0300

    answering to request with 'sendMessage' method

--- bot.py
+++ bot.py
@@ -1,15 +1,32 @@
+import aiohttp
 from aiohttp import web
+
+import config
 
 
 async def root(request):
-    data = await request.read()
-    return web.Response(text=f"Hello, world\n" + data.decode())
+    data = await request.json()
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    message = {
+        'chat_id': data['message']['chat']['id'],
+        'text': data['message']['text'],
+    }
+    url = f'{config.API}/sendMessage'
+    params = dict(url=url, json=message, headers=headers)
+    try:
+        async with aiohttp.request('POST', **params) as response:
+            if response.status == 200:
+                return web.Response()
+    except aiohttp.client_exceptions.ClientError:
+        pass
+    return web.Response(status=500)
 
 
 def main():
     app = web.Application()
     app.add_routes([
-        web.get('/', root),
         web.post('/', root),
     ])
     web.run_app(app, host='0.0.0.0', port=8081)
