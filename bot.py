7acd62891f3bb0362fdcd3cce631c7080be79bff
artslob <artslob@yandex.ru>
2018-12-31 19:45:24 +0300

    add webhook registration on start up

--- bot.py
+++ bot.py
@@ -1,7 +1,36 @@
+import asyncio
+
 import aiohttp
 from aiohttp import web
 
 import config
+
+
+def webhook_address():
+    return f'/{config.TOKEN}'
+
+
+async def register_webhook():
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'url': f'{config.HOST}{webhook_address()}',
+    }
+    url = f'{config.API}/setWebhook'
+    params = dict(url=url, json=data, headers=headers)
+
+    for _ in range(config.WEBHOOK_RETRIES):
+        try:
+            async with aiohttp.request('POST', **params) as response:
+                if response.status != 200:
+                    continue
+                result = await response.json()
+                if result['ok']:
+                    return True
+        except aiohttp.client_exceptions.ClientError:
+            pass
+    return False
 
 
 async def webhook(request):
@@ -25,9 +54,14 @@ async def webhook(request):
 
 
 def main():
+    loop = asyncio.get_event_loop()
+    webhook_registered = loop.run_until_complete(register_webhook())
+    if not webhook_registered:
+        print('Could not register webhook')
+        return
     app = web.Application()
     app.add_routes([
-        web.post('/', webhook),
+        web.post(webhook_address(), webhook),
     ])
     web.run_app(app, host='0.0.0.0', port=8081)
 
