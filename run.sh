48eb0a27cb87d8542d333e612c9ae4d0b3f3e9f8
artslob <artslob@yandex.ru>
2018-12-31 15:29:48 +0300

    add run script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+project_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+venv_root="${HOME}/.virtualenvs/artslob-bot"
+
+cd "$project_root"
+
+if [[ ! -e "$venv_root" ]]; then
+    python3 -m venv "$venv_root"
+fi
+
+source "${venv_root}/bin/activate"
+python3 -m pip install -r requirments.txt
+python3 bot.py
+
