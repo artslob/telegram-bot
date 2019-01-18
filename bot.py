035173442c2d7bf0d65a428246b058555108515e
artslob <artslob@yandex.ru>
2019-01-18 11:55:52 +0300

    using logging in main script

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 import asyncio
+import logging
 
 import aiohttp
 from aiohttp import web
 
 import config
+import config.log
 from telegram import commands
 from telegram.methods import SendMessageMethod, SetWebhookMethod
 from telegram.objects import Update
+
+logger: logging.Logger
 
 
 def webhook_address():
@@ -32,6 +36,8 @@ async def register_webhook():
 async def webhook(request):
     try:
         await _webhook(request)
+    except Exception:
+        logger.exception('while processing webhook')
     finally:
         return web.Response(status=200)
 
@@ -41,24 +47,38 @@ async def _webhook(request):
     if not update.message:
         return  # ignore all messages except direct from users
 
+    chat_id = update.message.chat.id
+    name = update.message.chat.username
+    text = update.message.text
+    logger.info('got message from id: %s, username: %s with text %s', chat_id, name, text)
+
     message = {
-        'chat_id': update.message.chat.id,
-        'text': commands.execute_command(update.message.text),
+        'chat_id': chat_id,
+        'text': commands.execute_command(text),
     }
     async with SendMessageMethod.post_json(message):
         pass
 
 
 def main():
+    # init logging
+    global logger
+    config.log.init_log()
+    logger = logging.getLogger('webhook')
+
+    # register webhook
     loop = asyncio.get_event_loop()
     webhook_registered = loop.run_until_complete(register_webhook())
     if not webhook_registered:
-        print('Could not register webhook')
+        logger.error('could not register webhook')
         return
+
+    # start app
     app = web.Application()
     app.add_routes([
         web.post(webhook_address(), webhook),
     ])
+    logger.info('app started')
     web.run_app(app, host='0.0.0.0', port=8081)
 
 
