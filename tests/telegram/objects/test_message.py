74be71b5cba2374411fe1c352ef6a2e2c281df0b
artslob <artslob@yandex.ru>
2019-01-23 16:27:14 +0300

    add tests of message object

---
+++ tests/telegram/objects/test_message.py
@@ -0,0 +1,45 @@
+import pytest
+
+from telegram.objects import Message
+
+
+@pytest.fixture()
+def parse_message_dict(dict_parser):
+    return dict_parser(Message)
+
+
+def test_empty_dict(parse_message_dict):
+    with pytest.raises(TypeError, match='missing 3 required positional arguments'):
+        parse_message_dict({})
+
+
+def test_message(parse_message_dict):
+    message_dict = {
+        "date": 1441645532,
+        "chat": {
+            "last_name": "Test Lastname",
+            "type": "private",
+            "id": 1111111,
+            "first_name": "Test Firstname",
+            "username": "Testusername"
+        },
+        "message_id": 1365,
+        "from": {
+            "last_name": "Test Lastname",
+            "id": 1111111,
+            "first_name": "Test Firstname",
+            "username": "Testusername"
+        },
+        "text": "/start",
+    }
+    message = parse_message_dict(message_dict)
+    assert message.message_id == 1365
+    assert message.date == 1441645532
+    assert message.chat is not None
+    assert message.chat.id == 1111111
+    assert message.chat.type == 'private'
+    assert message.chat.username == 'Testusername'
+    assert message.chat.first_name == 'Test Firstname'
+    assert message.chat.last_name == 'Test Lastname'
+    assert message.from_user is None  # FIXME
+    assert message.text == '/start'
