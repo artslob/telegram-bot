248572ffbed99b5924a625d5e6aa786a744aa269
artslob <artslob@yandex.ru>
2019-02-02 15:09:12 +0300

    add static method to create telegram fields

--- telegram/objects/__init__.py
+++ telegram/objects/__init__.py
@@ -1,5 +1,5 @@
 from .common.base import TelegramObject
-from .common.fields import TelegramField, RequiredField, OptionalField
+from .common.fields import TelegramField, telegram_field, RequiredField, OptionalField
 from .chat import Chat
 from .message import Message
 from .update import Update
--- telegram/objects/common/fields.py
+++ telegram/objects/common/fields.py
@@ -1,9 +1,25 @@
 from abc import ABC, abstractmethod
-
+from typing import TypeVar
 
 # TODO value can be list
 
 # TODO make fields static factories, without need to create instance at class level of telegram object (move to init)
+
+T = TypeVar('T')
+
+
+def telegram_field(factory: T, value, *, required=False) -> T:
+    base = True if issubclass(factory, (int, str, bool, float)) else False
+    if value is None and not required:
+        return None
+    if value is None and required:
+        raise ValueError('required value is None')
+
+    if base:
+        return factory(value)
+
+    return factory.from_dict(value)
+
 
 class TelegramField(ABC):
     def __init__(self, factory):
