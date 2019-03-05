044e3fde5261db5803098e306547056617fbe703
artslob <artslob@yandex.ru>
2019-03-05 15:00:46 +0300

    add tests of parsing cli args

---
+++ tests/test_config_args.py
@@ -0,0 +1,31 @@
+import pytest
+
+from config.args import get_parser
+
+
+@pytest.fixture
+def parse():
+    def _parse(args):
+        return get_parser().parse_args(args)
+
+    return _parse
+
+
+@pytest.mark.parametrize('number', ['8081', '80'])
+def test_port(parse, number):
+    assert parse(['-p', number]).port == int(number)
+
+
+@pytest.mark.parametrize('number', ['-80', '0'])
+def test_port_not_positive(parse, capsys, number):
+    with pytest.raises(SystemExit):
+        parse(['-p', number])
+    captured = capsys.readouterr()
+    assert captured.err.endswith('error: argument -p/--port: value should be positive\n')
+
+
+def test_port_not_integer(parse, capsys):
+    with pytest.raises(SystemExit):
+        parse(['-p', 'asd'])
+    captured = capsys.readouterr()
+    assert captured.err.endswith("error: argument -p/--port: not integer value: 'asd'\n")
