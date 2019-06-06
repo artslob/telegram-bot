41c8b7f7f37e60369a3fa219db665de7d3e52ece
artslob <artslob@yandex.ru>
2019-06-06 19:22:39 +0300

    refactor check for base class factory

--- telegram/objects/common/fields.py
+++ telegram/objects/common/fields.py
@@ -6,13 +6,12 @@ T = TypeVar('T')
 
 
 def telegram_field(factory: T, value, *, required=False) -> T:
-    base = True if issubclass(factory, (int, str, bool, float)) else False
     if value is None and not required:
         return None
     if value is None and required:
         raise ValueError('required value is None')
 
-    if base:
+    if issubclass(factory, (int, str, bool, float)):
         return factory(value)
 
     return factory.from_dict(value)
